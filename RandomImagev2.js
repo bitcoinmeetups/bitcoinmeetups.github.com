348d06b307a293b6b1716abfa335dc0bc9e7ae4f
Entrances <Entrances@users.noreply.github.com>
2015-02-16 07:16:15 +0000

    Update RandomImagev2.js

--- RandomImagev2.js
+++ RandomImagev2.js
@@ -1,3 +1,7 @@
-var RndNr = Math.floor(Math.random() * 5); var ia = ["http://pixabay.com/static/uploads/photo/2014/04/03/10/52/robot-311557_640.png",
-"http://pixabay.com/static/uploads/photo/2013/07/18/10/57/sphere-163623_640.jpg", "http://pixabay.com/static/uploads/photo/2014/11/02/16/05/electronic-devices-514182_640.jpg", "Collage/supermarketchiller.svg", "Backgroundv3.svg"]; document.getElementById("RndImg").src =
-ia[RndNr]
+setInterval(function(){  
+
+var RndNr = Math.floor(Math.random() * 3); var ia = ["http://pixabay.com/static/uploads/photo/2014/04/03/10/52/robot-311557_640.png",
+"http://pixabay.com/static/uploads/photo/2013/07/18/10/57/sphere-163623_640.jpg", "http://pixabay.com/static/uploads/photo/2014/11/02/16/05/electronic-devices-514182_640.jpg"]; document.getElementById("RndImg").src =
+ia[RndNr];
+
+}, 10000);
