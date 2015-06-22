86bfad7dbdb13a0a0694c37418421bbda2b1ea81
PB <go9db@boun.cr>
2015-06-22 12:35:13 +0700

    New commit

--- random-image-v4.js
+++ random-image-v4.js
@@ -1,11 +1,18 @@
 setInterval(function(){  
 
-var RndNr = Math.floor(Math.random() * 3); var ia = [
+var RndNr = Math.floor(Math.random() * 10); var ia = [
 
 
 "https://pbs.twimg.com/media/CHb6Z2eXAAEvQxC.jpg:large",
 "https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png", 
-"https://pbs.twimg.com/media/CHRZOl0WgAAOLpW.jpg"
+"https://pbs.twimg.com/media/CHyU_GIWoAE2ubN.png",
+"https://pbs.twimg.com/media/CHyGQw8WUAAkL2D.jpg",
+"https://pbs.twimg.com/media/CHxgc4wWoAAm9HC.jpg",
+"https://pbs.twimg.com/media/CHxeXYzWUAARQbg.jpg",
+"https://pbs.twimg.com/media/CHtF60WXAAAJKZh.jpg",
+"https://pbs.twimg.com/media/CHs8XcnWUAA0hZn.jpg",
+"https://pbs.twimg.com/media/CHqkncKWoAAwW7K.jpg",
+"https://pbs.twimg.com/media/CHOn58cWkAE6a4a.png"
 
 ]; 
 
@@ -16,12 +23,18 @@ ia[RndNr];
 
 setInterval(function(){  
 
-var RndNr = Math.floor(Math.random() * 3); var ia = [
+var RndNr = Math.floor(Math.random() * 10); var ia = [
 
 "https://pbs.twimg.com/media/CHb6Z2eXAAEvQxC.jpg:large",
 "https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png", 
-"https://pbs.twimg.com/media/CHRZOl0WgAAOLpW.jpg"
-
+"https://pbs.twimg.com/media/CHyU_GIWoAE2ubN.png",
+"https://pbs.twimg.com/media/CHyGQw8WUAAkL2D.jpg",
+"https://pbs.twimg.com/media/CHxgc4wWoAAm9HC.jpg",
+"https://pbs.twimg.com/media/CHxeXYzWUAARQbg.jpg",
+"https://pbs.twimg.com/media/CHtF60WXAAAJKZh.jpg",
+"https://pbs.twimg.com/media/CHs8XcnWUAA0hZn.jpg",
+"https://pbs.twimg.com/media/CHqkncKWoAAwW7K.jpg",
+"https://pbs.twimg.com/media/CHOn58cWkAE6a4a.png"
 
 ]; document.getElementById("RndImg2").src =
 ia[RndNr];
