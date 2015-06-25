829180f9b7c501f94b6c1be252497ca50246e288
PB <go9db@boun.cr>
2015-06-25 13:01:22 +0700

    New commit

---
+++ random-image-and-word.js
@@ -0,0 +1,61 @@
+setInterval(function(){  
+
+var RndNr = Math.floor(Math.random() * 10); var ia = [
+
+"https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png",
+"https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png", 
+"https://pbs.twimg.com/media/CHyU_GIWoAE2ubN.png",
+"https://pbs.twimg.com/media/CHyGQw8WUAAkL2D.jpg",
+"https://pbs.twimg.com/media/CHxgc4wWoAAm9HC.jpg",
+"https://pbs.twimg.com/media/CHxeXYzWUAARQbg.jpg",
+"https://pbs.twimg.com/media/CHtF60WXAAAJKZh.jpg",
+"https://pbs.twimg.com/media/CHs8XcnWUAA0hZn.jpg",
+"https://pbs.twimg.com/media/CHqkncKWoAAwW7K.jpg",
+"https://pbs.twimg.com/media/CHOn58cWkAE6a4a.png"
+
+]; 
+
+document.getElementById("RndImg").src =
+ia[RndNr];
+
+}, 20000);
+
+setInterval(function(){  
+
+var RndNr = Math.floor(Math.random() * 10); var ia = [
+
+"https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png",
+"https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png", 
+"https://pbs.twimg.com/media/CHyU_GIWoAE2ubN.png",
+"https://pbs.twimg.com/media/CHyGQw8WUAAkL2D.jpg",
+"https://pbs.twimg.com/media/CHxgc4wWoAAm9HC.jpg",
+"https://pbs.twimg.com/media/CHxeXYzWUAARQbg.jpg",
+"https://pbs.twimg.com/media/CHtF60WXAAAJKZh.jpg",
+"https://pbs.twimg.com/media/CHs8XcnWUAA0hZn.jpg",
+"https://pbs.twimg.com/media/CHqkncKWoAAwW7K.jpg",
+"https://pbs.twimg.com/media/CHOn58cWkAE6a4a.png"
+
+]; document.getElementById("RndImg2").src =
+ia[RndNr];
+
+}, 20000);
+
+setInterval(function(){  
+
+var RndWord = Math.floor(Math.random() * 10); var rw = [
+
+"https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png",
+"https://pbs.twimg.com/media/CGpyf3AUkAEBdW6.png", 
+"https://pbs.twimg.com/media/CHyU_GIWoAE2ubN.png",
+"https://pbs.twimg.com/media/CHyGQw8WUAAkL2D.jpg",
+"https://pbs.twimg.com/media/CHxgc4wWoAAm9HC.jpg",
+"https://pbs.twimg.com/media/CHxeXYzWUAARQbg.jpg",
+"https://pbs.twimg.com/media/CHtF60WXAAAJKZh.jpg",
+"https://pbs.twimg.com/media/CHs8XcnWUAA0hZn.jpg",
+"https://pbs.twimg.com/media/CHqkncKWoAAwW7K.jpg",
+"https://pbs.twimg.com/media/CHOn58cWkAE6a4a.png"
+
+]; document.getElementById("RndImg2").src =
+rw[RndWord];
+
+}, 20000);
